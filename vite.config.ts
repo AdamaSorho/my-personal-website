f92dfc6873540fae2fd63d957fb0810b9c6ac1ea
Adama Sorho <djomantcha@gmail.com>
2025-04-09 10:07:49 -0500

    updated base link

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/my-personal-website/",
   plugins: [react(), tailwindcss(), flowbiteReact()],
 });
